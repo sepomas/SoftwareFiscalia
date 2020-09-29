4ba12a5b4fef263dff9fefbb6a98495b8478404f
FJJI <fjjimenez@miuandes.cl>
2020-09-29 01:27:28 -0300

    Validates Para Rut, debe usar puntos (.) y guion (-), la K aun no la entra

--- app/models/thief.rb
+++ app/models/thief.rb
@@ -1,2 +1,4 @@
 class Thief < ApplicationRecord
+  validates :rut, format: { with: /([0-3]{0,1}+).([000-999]{3}+).([000-999]{3}+)-([0-9])/,
+                            message: 'invalid' }, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :rut, format: { with: /([0-3]{0,1}+).([000-999]{3}+).([000-999]{3}+)-([0-9])/,
+                            message: 'invalid' }, uniqueness: true
+
   def Carabinero?
     if job == "Carabinero"
       true
